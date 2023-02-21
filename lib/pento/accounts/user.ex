f095a00b6a0ecf067347f3cf315db3362cdc6c39
zwpdbh <hyperion_z@outlook.com>
2023-02-21 23:08:12 +0800

    > need to solve bug:   * 1st argument: not an atom

--- lib/pento/accounts/user.ex
+++ lib/pento/accounts/user.ex
@@ -4,6 +4,7 @@ defmodule Pento.Accounts.User do
 
   schema "users" do
     field :email, :string
+    field :username, :string
     field :password, :string, virtual: true, redact: true
     field :hashed_password, :string, redact: true
     field :confirmed_at, :naive_datetime
@@ -30,9 +31,17 @@ defmodule Pento.Accounts.User do
   """
   def registration_changeset(user, attrs, opts \\ []) do
     user
-    |> cast(attrs, [:email, :password])
+    |> cast(attrs, [:email, :username, :password])
+    |> validate_username()
     |> validate_email()
     |> validate_password(opts)
+  end
+
+  defp validate_username(changeset) do
+    changeset
+    |> validate_required([:username])
+    |> validate_length(:username, max: 50)
+    |> unique_constraint(:username)
   end
 
   defp validate_email(changeset) do
--- lib/pento_web/views/user_registration_view.ex
+++ lib/pento_web/views/user_registration_view.ex
@@ -1,3 +1,7 @@
 defmodule PentoWeb.UserRegistrationView do
   use PentoWeb, :view
+  def username_input(form, field, _opts \\ []) do
+    # form.input :username, opts
+    form.input field
+  end
 end
